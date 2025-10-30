a411c6c46e2b62af92ce4778d99282d4ead0caed
alexpetkoff <iskander9207@icloud.com>
2025-10-30 16:13:38 +0200

    Create merge_sort.py

---
+++ 3. Sorting Algorithms/merge_sort.py
@@ -0,0 +1,35 @@
+def merge_sort(nums):
+    nums_length = len(nums)
+    
+    if nums_length < 2:
+        return nums
+
+    middle_index = nums_length // 2
+    
+    first_list = nums[:middle_index]
+    second_list = nums[middle_index:]
+    
+    first_res = merge_sort(first_list)
+    second_res = merge_sort(second_list)
+
+    return merge(first_res, second_res)
+
+
+def merge(first, second):
+    final = []
+    i = 0
+    j = 0
+
+    while i < len(first) and j < len(second):        
+        if first[i] <= second[j]:
+            final.append(first[i])
+            i += 1
+        else:
+            final.append(second[j])
+            j += 1
+            
+    final.extend(first[i:])
+    final.extend(second[j:])
+    
+
+    return final
